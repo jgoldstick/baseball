abd07c69554d95c9b432ac0c47860ab4509fd491
Joel Goldstick <joel.goldstick@gmail.com>
2016-02-14 16:52:12 -0500

    new file:   .gitignore new file:   .project deleted:    baseball_stats/baseball/temp.py modified:   baseball_stats/baseball/templates/baseball/top_menu.html modified:   baseball_stats/baseball/views.py modified:   baseball_stats/baseball_stats/.ropeproject/globalnames modified:   baseball_stats/baseball_stats/settings.py modified:   baseball_stats/baseball_stats/templates/base.html

--- baseball_stats/baseball_stats/settings.py
+++ baseball_stats/baseball_stats/settings.py
@@ -97,6 +97,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = (
+        os.path.join(BASE_DIR, "static"),
+)
 
 """
 LOGGING = {
